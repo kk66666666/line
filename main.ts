adaf0219f88c73b253748990c07ec760e2aa677f
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-10-18 22:32:38 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+basic.clearScreen()
+let x = 0
+let y = 0
+let index = 4
+for (let index2 = 0; index2 < 5; index2++) {
+    for (let y = 0; y <= index; y++) {
+        led.plot(x, y)
+    }
+    basic.pause(100)
+    index += -1
+    x += 1
+}
